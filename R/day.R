90cef72945c495373d7faf9db95b3fe19d66b0b0
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2011-05-06 16:44:08 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/day.R
+++
@@ -1,5 +0,0 @@
-day <- function (x) UseMethod("day")
-
-day.default <- function(x){
-	as.numeric(format(x, "%j"))
-}
--- R/week.num.R
+++
@@ -1,9 +0,0 @@
-week.num <- function (x,...) UseMethod("week.num")
-
-week.num.default <- function(x, day="sunday")
-	{
-	 if(day %nin% c("monday","sunday")) {stop("day must be 'monday' or 'sunday'")}
-	 if(day=="sunday") {out <-as.numeric(format(x, "%U"))}
-   if(day=="monday") {out <-as.numeric(format(x, "%W"))}
-	 out	
-	}
