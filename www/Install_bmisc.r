8fe4133cefa80a1b1e89110d8e0a9f6d47fbd010
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2011-05-09 17:30:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/Install_bmisc.r
@@ -0,0 +1,19 @@
+# Define repository from with to search for packages
+options(repos=c("http://cran.skazkaforyou.com/","http://R-Forge.R-project.org"))
+
+# Function to install missing packages from binary 
+
+  
+# Find out what bmisc needs that is not already installed
+necessary <- c("car","lattice", "zoo", "robustbase", "methods", "bmisc")
+
+# Find out what is already installed on the local computer
+installed <- necessary %in% installed.packages()[, 'Package']
+
+# Update what is already installed and necessary for bmisc to work
+if (length(necessary[installed]) >=1)
+  update.packages(necessary[installed])
+
+# Install what is not already installed and necessary for bmisc to work
+if (length(necessary[!installed]) >=1)
+  install.packages(necessary[!installed], dependencies=T)
