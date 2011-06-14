9a9a5a55b13fb44a1fe89f771e9b7c0628e7e5ac
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2011-06-14 01:40:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bmisc/R/roundup.R
+++
@@ -1,8 +0,0 @@
-roundup <- function (x,...) UseMethod("roundup")
-
-roundup.default <- function(x,numdigits=0){
-	x <- x * 10^numdigits
-	x <- ifelse(x<0,-trunc(abs(x)+0.5),trunc(x+0.5))
-	x / 10^numdigits
-}
-
--- pkg/bmisc/R/week.1.R
+++
@@ -1,6 +0,0 @@
-week.1 <- function (x) UseMethod("week.1")
-
-week.1.default <- function(x)
-	{
-		(day(x)-1) %/% 7 +1
-	}
