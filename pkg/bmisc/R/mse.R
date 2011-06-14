a17d8d138c87be5a82c26c860aaad5b877099a09
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2011-06-14 01:57:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bmisc/R/mse.R
+++
@@ -1,6 +0,0 @@
-mse <- function (model) UseMethod("mse")
-
-mse.default <- function(model){
-	mean(residuals(model,type ="response")^2)/df.residual(model)
-}
-
--- pkg/bmisc/R/rm.levels.R
+++
@@ -1,6 +0,0 @@
-rm.levels <- function (factor) UseMethod("rm.levels")
-
-rm.levels.default <- function(factor){
-	as.factor(as.character(factor))
-}
-
