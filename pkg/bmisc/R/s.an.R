cd18f5a574fe0417f5050cccdc5c61365aae7173
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2011-07-21 19:38:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bmisc/R/s.an.R
+++ pkg/bmisc/R/s.an.R
@@ -4,10 +4,9 @@
 # Author: bruneaub                                                            #
 ###############################################################################
 
-s.an=function(object,mat=c(800,1100,50), fsel.max=NA, fsel.min=NA, M=NA,last.age=NA, f.MSP=NA, lstart=NA){
-    
-    
-    parms=list(mat=mat, fsel.max=fsel.max, fsel.min=fsel.min, M=M,last.age=last.age, f.MSP=f.MSP, lstart=lstart)
+s.an=function(object,mat=c(800,1100,50), fisher.sel=NA ,fsel=NA, fsel.min=NA, M=NA,last.age=NA, f.MSP=NA, lstart=NA){
+        
+    parms=list(mat=mat, fsel=fsel, fsel.min=fsel.min, M=M,last.age=last.age, f.MSP=f.MSP, lstart=lstart)
     if(class(object)!="ypr") stop("'object' must be of class 'ypr' resulting from ypr.l() function. Read help('ypr.l').")
     if(all(is.na(parms)))
         stop("\nSensitivity analysis can not be done without\n specifying which parameter to test.\n")
@@ -22,6 +21,7 @@ s.an=function(object,mat=c(800,1100,50), fsel.max=NA, fsel.min=NA, M=NA,last.age
         
         cat(paste("The '", names(parm),"' parameter in the ypr object is of type '",
                         object@parms$mat[[1]],"'. Read help('ypr.i')\n", sep=""))
+        if(object@parms$mat[[1]]!='full')stop("Sensitivity anlysis can only be done for maturity of type 'full' for now.")
         parm.vals=parm[[1]]
         parm.seq=seq(parm.vals[1],parm.vals[2],by=parm.vals[3])
         parms.seq.n=seq(n(parm.seq))
@@ -45,6 +45,35 @@ s.an=function(object,mat=c(800,1100,50), fsel.max=NA, fsel.min=NA, M=NA,last.age
             cat(paste(n(parm.seq)-i,"left\n"))
         }
     }
+    
+    if(names(parm)=='fsel') {
+        cat(paste("The '", names(parm),"' parameter in the ypr object is of type '",
+                        object@parms$mat[[1]],"'. Read help('ypr.i')\n", sep=""))
+        
+        parm.vals=parm[[1]]
+        parm.seq=seq(parm.vals[1],parm.vals[2],by=parm.vals[3])
+        parms.seq.n=seq(n(parm.seq))
+        
+        diff.parm=parm.seq-object@parms$fsel.type[3]
+        
+        refs=list(NULL)
+        ref.line.sels=list(NULL)
+        for(i in parms.seq.n){
+            k=parm.seq[i]
+            name=paste("ypr.mat",k, sep=".")
+            assign(name,ypr.l(fsel.type=object@parms$fsel.type, vonB=object@parms$vonB,  LW=object@parms$LW,
+                            l.start=object@parms$l.start,last.age=object@parms$last.age,age.step=object@parms$age.step,
+                            F.max=object@parms$F.max, F.incr.YPR=object@parms$F.incr.YPR, M=object@parms$M,
+                            f.MSP=object@parms$f.MSP, riv.calc=object@parms$riv.calc, F.f=object@parms$F.f, M.f=object@parms$M.f,
+                            mat=list(object@parms$mat[[1]],k))
+            
+            )
+            refs[[i]]=get(name)@ref
+            names(refs)[[i]]=name
+            #ref.line.sels[[i]]=get(name)@ref.line.sel
+            #names(ref.line.sels)[[i]]=name
+            cat(paste(n(parm.seq)-i,"left\n"))
+            
     title=paste(object@title,"\n   sensitivity analysis for '", names(parm),"' in ",
             parm.vals[1]," to ",parm.vals[2], " by ",parm.vals[3],sep="")
     res=new("ypr.sens",
