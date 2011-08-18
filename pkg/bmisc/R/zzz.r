874ff277e0d74910f591a7564bbe147919887df5
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2011-08-18 19:38:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bmisc/R/zzz.r
+++ pkg/bmisc/R/zzz.r
@@ -1,20 +1,22 @@
-.First.lib <- function(lib, pkg)
+.First.lib <- function(libname, pkgname)
 {
-
-	pkg_info <- utils::sessionInfo( package=pkg )$otherPkgs[[ pkg ]]
-	pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
-	cat("
-bmisc",pkg_info$Version,"
-
---------------------------------------------------------------
-bmisc comes with ABSOLUTELY NO WARRANTY;
-It has different functions that I have accumulated with time.
-I am not the author of all of them even though I have 
-modified most of them. This is an Alpha version.
---------------------------------------------------------------
-
-To view the user's guide 'bmisc.pdf', type vignette(\"bmisc\").
-It is still under construction.
-
-Packaged on ",pkg_date)
+        
+        pkg_info <- utils::sessionInfo( package=pkgname )$otherPkgs[[ pkgname ]]
+        pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
+        packageStartupMessage("
+                        bmisc",pkg_info$Version,"
+                        
+                        --------------------------------------------------------------
+                        bmisc comes with ABSOLUTELY NO WARRANTY;
+                        It has different functions that I have accumulated with time.
+                        I am not the author of all of them even though I have 
+                        modified most of them. This is an Alpha version.
+                        --------------------------------------------------------------
+                        
+                        To view the user's guide 'bmisc.pdf', type vignette(\"bmisc\").
+                        It is still under construction.
+                        
+                        Packaged on ",pkg_date)
+        
+        
 }
