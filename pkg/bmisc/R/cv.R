8b3fcb05b1bc228b17f4df0f95ee695401e593c7
benlafleche (Benoit Bruneau) <benlafleche@gmail.com>
2012-02-20 20:37:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bmisc@201 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/bmisc/R/cv.R
+++ pkg/bmisc/R/cv.R
@@ -2,5 +2,12 @@ cv <- function (x,...) UseMethod("cv")
 
 cv.default <-
 function(x, na.rm=T){
-	sd(x,na.rm=na.rm)/abs(mean(x,na.rm=na.rm))
+	sd(x,na.rm=na.rm)/mean(x,na.rm=na.rm)
+}
+
+rsd <- function (x,...) UseMethod("rsd")
+
+rsd.default <-
+function(x, na.rm=T){
+	abs(sd(x,na.rm=na.rm)/mean(x,na.rm=na.rm))
 }
